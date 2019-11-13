36b80a11a689fad1b0c9a1d56e20f8b218e3c342
Gabriel Seals <sealsgj@gmail.com>
2019-11-12 18:51:20 -0600

    adding to store

--- src/javascripts/helpers/data/pins.js
+++ src/javascripts/helpers/data/pins.js
@@ -7,11 +7,16 @@ const baseUrl = apiKeys.firebaseKeys.databaseURL;
 const getPinsByBoardID = (boardId) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/pins.json?orderBy="boardId"&equalTo="${boardId}"`)
     .then((response) => {
+      console.log('response', response.data === {});
+      console.log('object.keys', Object.keys.length);
       const demPins = response.data;
       if (demPins === {}) {
+        console.log('demPins is an empty object', demPins);
         const newString = 'There are no pins to display!';
         util.printToDom('singleBoard', newString);
       } if (demPins !== {}) {
+        console.log('demPins is exists', demPins);
+        console.log('object.keys in the second', Object.keys.length);
         const pins = [];
         Object.keys(demPins).forEach((pinId) => {
           demPins[pinId].id = pinId;
